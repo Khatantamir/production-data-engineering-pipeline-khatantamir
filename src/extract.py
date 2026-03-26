ec893d6eb380979a175f90e58db6705dec6c68dd
Khatantamir Otgonbyamba <hhatnaa690@gmail.com>
2026-03-27 00:34:51 +0800

    Update extract.py

--- src/extract.py
+++ src/extract.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+def extract():
+    url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv"
+    df = pd.read_csv(url)
+    df.to_csv("data/raw/raw.csv", index=False)
+    print("Data extracted!")
+
+if __name__ == "__main__":
+    extract()

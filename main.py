dfcfb5b55e95eeaec4467cee491009f8801ddb52
Khatantamir Otgonbyamba <hhatnaa690@gmail.com>
2026-03-27 01:08:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+import logging
 from src.extract import extract
 from src.transform import transform
 from src.load import load
 
+logging.basicConfig(level=logging.INFO)
+
+def main():
+    try:
+        logging.info("Starting pipeline...")
+
+        extract()
+        logging.info("Data extracted")
+
+        transform()
+        logging.info("Data transformed")
+
+        load()
+        logging.info("Data loaded")
+
+    except Exception as e:
+        logging.error(f"Pipeline failed: {e}")
+
 if __name__ == "__main__":
-    extract()
-    transform()
-    load()
+    main()
